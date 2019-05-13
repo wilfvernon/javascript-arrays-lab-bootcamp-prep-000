a28f164025ab917ef1db34d202fd58652bdec5f5
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-05-13 18:27:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
 
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
