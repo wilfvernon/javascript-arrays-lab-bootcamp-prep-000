c2cf8bf6ea6ca292d758b07c445c5c9845120371
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-05-13 18:36:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,23 @@ function destructivelyRemoveLastKitten(){
 function destructivelyRemoveFirstKitten(){
   return kittens.shift()
 }
+
 //conservative functions//
+function appendKitten(name){
+  new_array = [...kittens, name]
+  return new_array
+}
+
+function prependKitten(name){
+  new_array = [name, ...kittens]
+  return new_array
+}
+
+function removeLastKitten(){
+  new_array = kittens.slice(0, kittens.length - 1)
+  return new_array
+}
+
+function removeFirstKitten(){
+  new_array = kittens.slice(1)
+}
